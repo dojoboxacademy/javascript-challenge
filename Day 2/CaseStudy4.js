7406fe5d27e704437d7bdd9bb0a52b096cfcee60
Jonathan Frelispradana Musila <jonathanfrelis672@gmail.com>
2021-08-10 09:01:41 +0700

    Split files & add day 2 challenges code files

---
+++ Day 2/CaseStudy4.js
@@ -0,0 +1,23 @@
+/*
+
+Berapa berat badan ideal Anda?
+
+Rumus menghitung
+Pria: Berat badan ideal (kilogram) = [tinggi badan (sentimeter) - 100] - [(tinggi badan (sentimeter) - 100) x 10%]
+Wanita: Berat badan ideal (kilogram) = [tinggi badan (sentimeter) - 100] - [(tinggi badan (sentimeter) - 100) x 15%]
+
+nb = silahkan menggunakan input tinggi yang sama untuk menghitung kedua berat badan ideal.
+
+Output:
+Berat badan ideal pria untuk tinggi ... sentimeter adalah ... kilogram
+Berat badan ideal wanita untuk tinggi ... sentimeter adalah ... kilogram 
+
+*/
+
+function beratBadanPria() {
+
+}
+
+function beratBadanWanita() {
+    
+}
---
+++ Day 2/CaseStudy5.js
@@ -0,0 +1,25 @@
+/*
+
+Berapa berat badan ideal Anda?
+
+Rumus menghitung
+Pria: Berat badan ideal (kilogram) = [tinggi badan (sentimeter) - 100] - [(tinggi badan (sentimeter) - 100) x 10%]
+Wanita: Berat badan ideal (kilogram) = [tinggi badan (sentimeter) - 100] - [(tinggi badan (sentimeter) - 100) x 15%]
+
+nb = silahkan menggunakan input tinggi berdasarkan jenis kelamin Anda masing-masing.
+
+Output:
+(Pria)
+Berat badan ideal pria untuk tinggi ... sentimeter adalah ... kilogram
+(Wanita)
+Berat badan ideal wanita untuk tinggi ... sentimeter adalah ... kilogram
+
+*/
+
+function beratBadanPria() {
+
+}
+
+function beratBadanWanita() {
+    
+}
